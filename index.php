b6edd9346b7fee17ea634a6044a76575ae3269e1
wojtsal <76950270+wojtsal@users.noreply.github.com>
2022-07-01 11:59:15 +0200

    install

---
+++ index.php
@@ -0,0 +1,59 @@
+<!-- This is page for you to listen ANONE -->
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ANONE</title>
+    <!-- <link rel="stylesheet" href="./css/style.css" /> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Import dodatkowych pakietów do strony html -->
+    <link href="css/style.css" rel="stylesheet">
+    <script src="js/script.js"></script>
+    
+</head>
+
+<body>
+    <!-- Cała strona -->
+    <div>
+        <!-- Menu -->
+        <div class="menu">
+                <div class="column" id="column1">
+                    <a href="#">link</a>
+                </div>
+
+                <div class="column" id="column2">
+                    <a href="https://soundcloud.com/wwwanoone">soundcloud/wwwanoone</a> 
+                </div>
+
+                <div class="column" id="column3">
+                    <a href="https://www.instagram.com/www_anoone/">@www_anoone</a>
+                </div>
+                  
+        </div>
+        <div class="content">
+            <div class="images">    
+                <img src="images/nadmiar.jpeg" width="200px">
+                <a class="song" href="https://soundcloud.com/wwwanoone/nadmiar">Nadmiar</a>
+            </div>
+
+            <div class="images">
+                <img src="images/blood_money.jpeg" width="200px">
+                <a class="song" href="https://soundcloud.com/wwwanoone/blood-money">Blood Money</a>
+            </div>
+
+            <div class="images">
+                <img src="images/benchbitch.png" width="200px">
+                <a class="song" href="https://soundcloud.com/wwwanoone/miliony">Miliony</a>
+            </div>
+        </div>
+
+        
+
+    </div>
+    
+    
+
+</body>
+</html> 
